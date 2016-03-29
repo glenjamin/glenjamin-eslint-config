04e9a488cd6b89f57970260ce3d0772304bcfecc
Glen Mailer <glenjamin@gmail.com>
2016-03-29 15:55:46 +0100

    Add an example of var usage

---
+++ default/good/var.js
@@ -0,0 +1,10 @@
+// one-var is allowed, value optional
+var a = 1, b;
+
+// separate vars are allowed too, value optional
+var c = 1;
+var d;
+
+
+// Use the variables, to avoid warnings
+d += a * b * c;
